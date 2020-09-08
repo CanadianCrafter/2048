dbd01f2c86e386e181d61f17ceef304d236b2035
CanadianCrafter <bryan335667879@gmail.com>
2020-09-07 20:21:45 -0400

    final

--- src/EndScreen.java
+++ src/EndScreen.java
@@ -1,3 +1,4 @@
+//imports
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -5,37 +6,32 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 import javax.swing.*;
-
 import audio.MusicPlayer;
 
-
-
 public class EndScreen extends JFrame implements ActionListener {
+	//gui stuff
 	JPanel splashScreen = new JPanel();
 	JButton screenButton = new JButton();
 
-	// constructor method
+	//constructor method
 	public EndScreen() {
-
 		MusicPlayer.playClip("Voltorb Flip Game Over.wav");
 		frameSetup();
 		panelDesign();
-
 	}
 
-	// set up the frame
+	//set up the frame
 	private void frameSetup() {
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		setSize(500, 500);
 		setLayout(null);
-		add(splashScreen); // add to screen
+		add(splashScreen);
 		setVisible(true);
 		setResizable(false);
 	}
 
-	// set up the panel
+	//set up the panel
 	private void panelDesign() {
-		
 		//add features for the screen
 		splashScreen.setBorder(null);
 		splashScreen.setBackground(Color.BLACK);
@@ -44,18 +40,16 @@ public class EndScreen extends JFrame implements ActionListener {
 
 		//add features for the button
 		screenButton.addActionListener(this);
-		screenButton.setBounds(0, 0, 500, 500); // sets size and location; location moves so buttons don't over
+		screenButton.setBounds(0, 0, 500, 500); //this button fills the entire screen
 		screenButton.setIcon(new ImageIcon(new ImageIcon("Images/End Screen.png").getImage().getScaledInstance(500, 500, 0)));
-		splashScreen.add(screenButton); // add the button to the screen
+		splashScreen.add(screenButton); 
 
 	}
 
-	// this method opens another GUI when clicked the screen
+	//starts a new game, and skips the starting splash screen
 	public void actionPerformed(ActionEvent arg0) {
 		TwentyFortyEight.frame.dispose();
 		dispose();
-		new TwentyFortyEight(false,false,false); // open the next GUI
-		// new MazeRaceGUI();
-
+		new TwentyFortyEight(false,false,false); //starts a new game without saves
 	}
 }
